24522c84192e1e64bdbd90014ecade567bfe3c2b
Junior Moraes <contatomoraesjr@gmail.com>
2020-08-14 15:12:58 -0300

    First commit

---
+++ curso-jsf-primefaces-essencial/src/main/java/hellogithub/HelloGit.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package hellogithub;
+
+/**
+ * @author juniormoraes
+ * Just to genereate a firt Github commit
+ */
+public class HelloGit {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		System.out.println("Just hello Github");
+
+	}
+
+}
